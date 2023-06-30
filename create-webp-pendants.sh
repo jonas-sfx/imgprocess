4118053655e08e8572b8a69717d031211087477d
Jonas Hess <jonas@sfxonline.de>
2023-06-30 02:24:08 +0200

    add script create-webp-pendants.sh

---
+++ create-webp-pendants.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## filename     create-webp-pendants.sh
+##
+## description: run through a directory and
+##              create webp-pendant-files for each jpg/png-file
+##              and check their http-status-code
+##
+## requirements: cwebp-library. see build-cwebp.sh
+##
+## author:      jonas@sfxonline.de
+## =======================================================================
+
+path="$HOME/Pictures/uhu"
+
+for D in `find "$path" -type d`; do
+  for i in $D/*.{jpg,jpeg,png,JPG,JPEG,PNG} ;do
+    if [ ! -f "${i%.*}.webp" ] && [ -f "$i" ]; then
+        cwebp "$i" -o "${i%.*}.webp";
+    fi
+  done;
+done;
